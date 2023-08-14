e0e881c30acd48c8310668ca43eaa9da56880128
dino9881 <whdghthdms44@gmail.com>
2023-08-15 01:12:26 +0900

    [NOMAD-22] feat: IMac entity 생성

---
+++ src/main/java/nomad/backend/IMac/IMac.java
@@ -0,0 +1,29 @@
+package nomad.backend.IMac;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "imac")
+@Getter
+@Setter
+public class IMac {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer imac_id;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(name = "cadet")
+    private String cadet;
+
+    @Column(name = "logout_time")
+    private Date logout_time;
+
+    @Column(name = "left_cadet")
+    private Date left_cadet;
+}
