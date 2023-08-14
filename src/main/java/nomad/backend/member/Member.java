8b06514b0bd598efc39afc75656122a6209e8065
highcho <highjcho@gmail.com>
2023-08-15 01:44:29 +0900

    [NOMAD-22] feat: member 양방향 조회 및 생성자 추가, board entity 추가

--- src/main/java/nomad/backend/member/Member.java
+++ src/main/java/nomad/backend/member/Member.java
@@ -2,9 +2,15 @@ package nomad.backend.member;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nomad.backend.board.Board;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +18,14 @@ public class Member {
 
     @Column(nullable = false)
     private String intra;
+
+    @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
+    private List<Board> posts = new ArrayList<Board>();
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Starred> stars = new ArrayList<starred>();
+
+    public Member(String intra) {
+        this.intra = intra;
+    }
 }
