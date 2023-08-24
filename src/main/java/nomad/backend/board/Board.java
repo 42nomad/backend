a3c9fb4ce0ae95c881656778907d55ebceb0589a
highcho <highjcho@gmail.com>
2023-08-24 15:31:08 +0900

    [NOMAD-58, NOMAD-59] feat: board 관련 비즈니스 로직 및 리포지터리 생성

--- src/main/java/nomad/backend/board/Board.java
+++ src/main/java/nomad/backend/board/Board.java
@@ -3,7 +3,6 @@ package nomad.backend.board;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import nomad.backend.imac.IMac;
 import nomad.backend.member.Member;
 import java.util.Date;
 
@@ -12,15 +11,14 @@ import java.util.Date;
 @NoArgsConstructor
 public class Board {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long board_id;
+    private Long boardId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member writer;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "imac_id")
-    private IMac iMac;
+    @Column(nullable = false)
+    private String location;
 
     @Column(nullable = false)
     private String contents;
@@ -31,11 +29,17 @@ public class Board {
     @Column(nullable = false)
     private Date created_at;
 
-    public Board(Member member, IMac iMac, String contents, String image) {
+    public Board(Member member, String location, String contents, String image) {
         this.writer = member;
-        this.iMac = iMac;
+        this.location = location;
         this.contents = contents;
         this.image = image;
         this.created_at = new Date();
+    }
+
+    public void updatePost(WriteDto post) {
+        this.location = post.getLocation();
+        this.contents = post.getContents();
+        this.image = post.getImgUrl();
     }
 }
