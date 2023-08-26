0f25c4bb389906fec9c22b0951dc33423944eb6c
dino9881 <whdghthdms44@gmail.com>
2023-08-26 13:57:05 +0900

    feat: slack bot test 생성

--- src/main/java/nomad/backend/slack/Slack.java
+++ src/main/java/nomad/backend/slack/Slack.java
@@ -1,2 +1,11 @@
-package nomad.backend.slack;public class Slack {
+package nomad.backend.slack;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Slack {
+    String id;
+
 }
--- src/main/java/nomad/backend/slack/SlackService.java
+++ src/main/java/nomad/backend/slack/SlackService.java
@@ -1,2 +1,58 @@
-package nomad.backend.slack;public class SlackService {
+package nomad.backend.slack;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+@Service
+public class SlackService {
+    @Value(value = "${slack.token}")
+    String slackToken;
+    String slackChannel = "#자리_알림";
+
+    public void sendSlackMessage(String message) {
+
+        try {
+            MethodsClient methods = Slack.getInstance().methods(slackToken);
+            ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                    .channel(slackChannel)
+                    .text(message)
+                    .build();
+
+            methods.chatPostMessage(request);
+
+        } catch (SlackApiException | IOException e) {
+            System.out.println("error");
+        }
+    }
+
+    public String getSlackIdByEmail(String intrald) {
+        String url = "https://slack.com/api/users. lookupByEmail";
+        String email = intrald + "@gmail.com";
+        url += "?email=" + email;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + slackToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded");
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                requestEntity,
+                String.class
+        );
+        System.out.println(responseEntity);
+        return responseEntity.getBody();
+    }
 }
