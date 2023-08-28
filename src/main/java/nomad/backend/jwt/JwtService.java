ca059d5e5c6a29c62266ecc8dc56ad233b0c76e7
highcho <highjcho@gmail.com>
2023-08-28 17:49:20 +0900

    fix: cookie가 생성되지 않는 문제에 대해 쿠키 설정 변경

--- src/main/java/nomad/backend/jwt/JwtService.java
+++ src/main/java/nomad/backend/jwt/JwtService.java
@@ -100,9 +100,18 @@ public class JwtService {
         Cookie cookie = new Cookie("refresh", refreshToken);
         cookie.setHttpOnly(true);  //httponly 옵션 설정
         cookie.setSecure(true); //https 옵션 설정
-        cookie.setPath("/"); // 모든 곳에서 쿠키열람이 가능하도록 설정
+        cookie.setPath("http://localhost:3000/"); // 모든 곳에서 쿠키열람이 가능하도록 설정
         cookie.setMaxAge(60 * 60 * 24 * 14); //쿠키 만료시간 설정 2주
-        response.addCookie(cookie);
+
+        String cookieHeader = cookie.getName() + "=" + cookie.getValue()
+                + "; Secure"
+                + "; SameSite=None"
+                + "; HttpOnly"
+                + "; Path=" + cookie.getPath()
+                + "; Max-Age=" + cookie.getMaxAge();
+//        response.addCookie(cookie);
+
+        response.addHeader("Set-Cookie", cookieHeader);
     }
 
     public void updateRefreshToken(Long memberId, String refreshToken) {
