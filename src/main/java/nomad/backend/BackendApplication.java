cb0d548a52763097514dce90186cc105ffbf7f5f
highcho <highjcho@gmail.com>
2023-08-29 22:15:25 +0900

    fix: swagger cors 에러 해결을 위한 설정 추가

--- src/main/java/nomad/backend/BackendApplication.java
+++ src/main/java/nomad/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package nomad.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://api.42nomad.kr", description = "Default Server URL")})
 public class BackendApplication {
 
 	public static void main(String[] args) {
