52238f3382dd0198e063277a2dfc9afccbc0adbf
highcho <highjcho@gmail.com>
2023-08-30 16:59:54 +0900

    fix: file path 변경

--- src/main/java/nomad/backend/meetingroom/MeetingRoomService.java
+++ src/main/java/nomad/backend/meetingroom/MeetingRoomService.java
@@ -51,7 +51,7 @@ public class MeetingRoomService {
     @Transactional
     public void loadCsvDataToDatabase() throws IOException{
         String jarPath = FileHandler.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String filePath = Paths.get(jarPath).getParent() + "/home/ec2-user/apps/backend/src/main/java/nomad/backend/meetingroom/meetingroom.csv";
+        String filePath = Paths.get(jarPath).getParent() + "/home/ec2-user/apps/backend/src/main/java/nomad/backend/meetingroom/meetingRoom.csv";
         try (BufferedReader br = new BufferedReader(new FileReader(filePath, Charset.forName("UTF-8")))) {
             String line;
             while ((line = br.readLine()) != null) {
