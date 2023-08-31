512ffd10973fc48d64ea44ee7f683ffe5f2398fd
dino9881 <whdghthdms44@gmail.com>
2023-08-31 17:59:59 +0900

    fix: iotDto 생성 및 컨트롤러 수정

---
+++ src/main/java/nomad/backend/iot/IoTDto.java
@@ -0,0 +1,9 @@
+package nomad.backend.iot;
+
+import lombok.Getter;
+
+@Getter
+public class IoTDto {
+    private String name;
+    private String type;
+}
--- src/main/java/nomad/backend/iot/IotController.java
+++ src/main/java/nomad/backend/iot/IotController.java
@@ -3,6 +3,7 @@ package nomad.backend.iot;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import nomad.backend.board.WriteDto;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "IoTController", description = "게시판 컨트롤러")
@@ -12,19 +13,19 @@ import org.springframework.web.bind.annotation.*;
 public class IotController {
     @Operation(operationId = "iot", summary = "Iot test", description = "iot test")
     @GetMapping()
-    public String getIotTest(@RequestParam("name")String roomName,  @RequestParam("type") String clickType) {
+    public String getIotTest(@RequestBody IoTDto ioTDto) {
         System.out.println("get iot Test");
-        System.out.println(roomName);
-        System.out.println(clickType);
-        return roomName+ " " + clickType;
+        System.out.println(ioTDto.getName());
+        System.out.println(ioTDto.getType());
+        return ioTDto.getName()+ " " + ioTDto.getType();
     }
 
     @Operation(operationId = "iot", summary = "Iot test", description = "iot test")
     @PostMapping()
-    public String postIotTest(@RequestParam("name")String roomName,  @RequestParam("type") String clickType) {
+    public String postIotTest(@RequestBody IoTDto ioTDto) {
         System.out.println("get iot Test");
-        System.out.println(roomName);
-        System.out.println(clickType);
-        return roomName+ " " + clickType;
+        System.out.println(ioTDto.getName());
+        System.out.println(ioTDto.getType());
+        return ioTDto.getName()+ " " + ioTDto.getType();
     }
 }
