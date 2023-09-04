8f55d37f2f216a3fb64dd69c664fca695d118657
highcho <highjcho@gmail.com>
2023-09-04 13:22:38 +0900

    fix: 존재하지 않는api 요청시 401 에러가 반환되던 것을 404 에러로 수정

--- src/main/java/nomad/backend/global/handler/CustomAccessDeniedHandler.java
+++ src/main/java/nomad/backend/global/handler/CustomAccessDeniedHandler.java
@@ -3,6 +3,7 @@ package nomad.backend.global.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import nomad.backend.global.reponse.ResponseMsg;
 import nomad.backend.global.reponse.StatusCode;
 import org.springframework.http.HttpStatus;
@@ -13,17 +14,23 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final HttpRequestEndpointChecker endpointChecker;
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException e) throws IOException, ServletException {
-
-        String jsonResponse = "{\"statusCode\": " + StatusCode.FORBIDDEN + ",\"responseMsg\": \"" + ResponseMsg.FORBIDDEN +"\"}";
-
         response.setContentType("application/json;charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        response.getWriter().write(jsonResponse);
+        String jsonResponse = null;
+        if (endpointChecker.isEndpointExist(request)) {
+            jsonResponse = "{\"statusCode\": " + StatusCode.NOT_FOUND + ",\"responseMsg\": \"" + ResponseMsg.API_NOT_FOUND +"\"}";
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+        } else {
+            jsonResponse = "{\"statusCode\": " + StatusCode.FORBIDDEN + ",\"responseMsg\": \"" + ResponseMsg.FORBIDDEN + "\"}";
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+        }
+            response.getWriter().write(jsonResponse);
     }
 }
---
+++ src/main/java/nomad/backend/global/handler/HttpRequestEndpointChecker.java
@@ -0,0 +1,29 @@
+package nomad.backend.global.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+
+@Component
+@RequiredArgsConstructor
+public class HttpRequestEndpointChecker {
+    private final DispatcherServlet servlet;
+
+
+    public boolean isEndpointExist(HttpServletRequest request) {
+
+        for (HandlerMapping handlerMapping : servlet.getHandlerMappings()) {
+            try {
+                HandlerExecutionChain foundHandler = handlerMapping.getHandler(request);
+                if (foundHandler != null)
+                    return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
